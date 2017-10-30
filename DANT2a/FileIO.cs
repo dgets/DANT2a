e0421d6250a65843e6d14f34a775a02ea26f613f
Damo <damo.gets@gmail.com>
2017-10-30 13:21:21 -0500

    Starting work on #20
    Implementing binary serialization found at the link noted within the issue.  Just starting on this currently.

---
+++ DANT2a/FileIO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DANT2a{
+    class FileIO {
+        //set up a standard (per-user) location to store the active lists
+        public static void WriteActivesBinary<EntryType>(string path,
+            EntryType wutOne) {
+            using (Stream stream = File.Open(path, FileMode.Create)) {
+                var binFmttr = 
+                    new System.Runtime.Serialization.Formatters.Binary.
+                        BinaryFormatter();
+                binFmttr.Serialize(stream, wutOne);
+            }
+        }
+
+        public static EntryType ReadActivesBinary<EntryType>(string path) {
+            using (Stream stream = File.Open(path, FileMode.Open)) {
+                var binFmttr =
+                    new System.Runtime.Serialization.Formatters.Binary.
+                        BinaryFormatter();
+                return (EntryType)binFmttr.Deserialize(stream);
+            }
+        }
+    }
+}
