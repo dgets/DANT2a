efc66b5810106acedbe17058338e18f5ca02528f
Damo Gets <dgets@users.noreply.github.com>
2018-06-22 23:52:47 -0700

    Starting on issue (superset thereof) #25.
    Adding log & console beep capabilities.

--- DANT2a/FileIO.cs
+++ DANT2a/FileIO.cs
@@ -8,9 +8,11 @@ using System.Threading.Tasks;
 namespace DANT2a {
     class FileIO {
         private static String saveFile = "DANTentries.cfg";
+        private static String logFile = "DANT.log";
         private static String saveDir = Environment.GetFolderPath(
           Environment.SpecialFolder.ApplicationData);
         public static String saveDataLoc = saveDir + "\\" + saveFile;
+        public static String saveLogLoc = saveDir + "\\" + logFile;
 
         public static void WriteActivesXML<List>(string path,
           EntryType.AllEntries glob) {
@@ -44,6 +46,35 @@ namespace DANT2a {
 
               return (EntryType.AllEntries)xmlFmttr.Deserialize(stream);
             }
+        }
+
+        public static void DoLocInit() {
+            //we will definitely be needing to add some exception testing
+            //handle the saveDir schitt
+            if (File.GetAttributes(saveDir) != FileAttributes.Directory) {
+                Debug.ShowDbgOut("FileAttributes.Directory test on 'saveDir' is false");
+
+                //wipe the previous entry (maybe, or set behavior?), create new
+                //with the permissions that we need
+                Directory.CreateDirectory(saveDir); //don't forget try/catch
+            }
+
+            //handle the saveDataLoc schitt
+            if (File.GetAttributes(/*saveDir + saveFile*/ saveDataLoc) 
+                != FileAttributes.Normal) {
+                //same schitt here
+                Debug.ShowDbgOut("FileAttributes.Normal test on 'saveFile' is false");
+                File.CreateText(saveDataLoc);    //try/catch, etc etc
+            }
+
+            //handle the logLoc schitt
+            if (File.GetAttributes(saveLogLoc) != FileAttributes.Normal) {
+                //2nd verse, same as the first
+                Debug.ShowDbgOut("FileAttributes.Normal test on 'logFile' is false");
+                File.CreateText(saveLogLoc);    //try/catch, idiot (&)
+            }
+
+
         }
     }
 }
